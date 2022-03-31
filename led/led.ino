03c234da9a79725bf61b0e48c8bd6f54a6c13827
Oscar Rodriguez Rios <oscar.rodriguez02@correo.usa.edu.co>
2022-03-31 12:49:29 -0500

    Se agrega un condicional para saber a cuanta distancia se enciende el led

---
+++ led/led.ino
@@ -0,0 +1,20 @@
+int trig=13;
+int echo=12;
+int tiempo;
+int distancia;
+int rojo=7;
+
+void setup() {
+   Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(rojo,OUTPUT); 
+}
+void loop(){
+ if (distancia<20)
+  {
+    digitalWrite(rojo,LOW);
+  }
+
+
+}
