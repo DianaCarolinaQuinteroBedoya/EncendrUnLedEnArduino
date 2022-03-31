01ea6a19a5be3a16fc6fe7dde705d06a8da8bdbc
Juan Rodriguez <juans.rodriguezs@correo.usa.edu.co>
2022-03-31 12:45:28 -0500

    se agrego el codigo para tomar la lectura del echo y la muestra para saber la distancia que se encuentra el objeto

---
+++ led/led.ino
@@ -0,0 +1,22 @@
+int trig=13;
+int echo=12;
+int tiempo;
+int distancia;
+int rojo=7;
+
+void setup() {
+   Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(rojo,OUTPUT); 
+}
+void loop(){
+ digitalWrite(trig,HIGH);
+  delay(1);
+  digitalWrite(trig,LOW);
+  tiempo=pulseIn(echo,HIGH);
+  distancia=tiempo/58.2;
+  delay(500);
+  Serial.println(distancia);
+  
+}
